437d4590e50a970ed518c5b9a9a3951a36b74e72
lorenzoorsi <lorenzo.orsingher@gmail.com>
2024-10-11 11:35:03 +0200

    add custom pooler and pooling exps

--- NLU/part_2/model.py
+++ NLU/part_2/model.py
@@ -14,6 +14,7 @@ class MyBert(nn.Module):
         drop=0.1,
         model_name="bert-base-uncased",
         pooler=False,
+        custom_pooler=False,
     ):
         super(MyBert, self).__init__()
 
@@ -22,7 +23,20 @@ class MyBert(nn.Module):
         config = AutoConfig.from_pretrained(model_name)
         self.bert = AutoModel.from_pretrained(model_name)
 
+        # using CLS token pooler
         self.pooler = pooler
+        if self.pooler:
+            print("[MODEL] Using BERT pooler")
+
+        # custom pooler layer
+        self.custom_pooler = custom_pooler
+        if self.custom_pooler:
+            print("[MODEL] Using custom pooler")
+            self.bert.pooler = nn.Linear(config.hidden_size, intent_size)
+            self.bert.pooler_activation = nn.Tanh()
+
+        if self.pooler and self.custom_pooler:
+            print("[WARNING] Both pooler and custom pooler are in use")
 
         # config = self.bert.config
         self.dropout = nn.Dropout(drop)
@@ -43,11 +57,15 @@ class MyBert(nn.Module):
             cls_output = outputs.pooler_output
         else:
             cls_output = outputs.last_hidden_state[:, 0]
-        intent_logits = self.intent_classifier(self.dropout(cls_output))
+
+        if self.custom_pooler:
+            intent_logits = self.bert.pooler(cls_output)
+        else:
+            intent_logits = self.intent_classifier(self.dropout(cls_output))
 
         # Token-level embeddings for slot classification
         sequence_output = outputs.last_hidden_state
         slot_logits = self.slot_classifier(self.dropout(sequence_output))
         slot_logits = slot_logits.permute(0, 2, 1)
-        # breakpoint()
+        breakpoint()
         return intent_logits, slot_logits
